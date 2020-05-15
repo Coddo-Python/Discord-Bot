4f8582455900c821ee3f0258f3894420648819c8
Coddo <40121850+Manas-Likhe@users.noreply.github.com>
2020-05-15 23:05:21 +0400

    Update Bot.py
    First code added. Still testing locking system.

--- Bot.py
+++ Bot.py
@@ -1,1 +1,27 @@
+import discord
+from discord.ext import commands, tasks
+from discord.ext.tasks import loop
+import sys
+import random
 
+client = commands.Bot(command_prefix = '.')
+
+print("Program Started...")
+print("Bot Started...")
+print("Init Started...")
+StopCode = random.randint(0,9999)
+StopCode = str(StopCode)
+number = number(0)
+print(f"StopCode = {StopCode}")
+
+@client.event
+async def on_message(message):
+    if "/support" not in message.content or "/Support" not in message.content:
+        print("/support Activated")
+        number = number + 1
+        channel = await message.guild.create_text_channel(f"Support {number}")
+        channel = discord.utils.get(message.guild.channels, name = f"Support {number}", type = "ChannelType.text")
+        await channel.send('@Moderator')
+
+
+client.run('NzEwODUzNTU4ODI4MDA3NDQ3.Xr6gAA.2Aa5ciSSSkuzexPvY3b1fOAJ1Mc')
