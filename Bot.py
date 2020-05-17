80e7a09eb375876949814df286b990d06aa69c61
Coddo <40121850+Coddo-Python@users.noreply.github.com>
2020-05-17 12:33:47 +0400

    Update Bot.py
    Project Done

--- Bot.py
+++ Bot.py
@@ -17,23 +17,25 @@ prevmessage = None
 async def on_message(message):
     global lock #making sure that the variables work accross diferent functions
     global prevmessage
-    if lock == 1:
-        if prevmessage != message.content: #if the previous message is the same as the new one, nothing should hapen
-            channel = client.get_channel(711137574642516008) #McChat Channel
-            await channel.send(message.content) #Mimic Message
-            prevmessage = message.content #Part of system to stop dupes
-            await asyncio.sleep(2) #To Avoid bot overload
-    if "/support" in message.content.lower():
-        print("/support Activated")
-        channel = client.get_channel(710934053901303828)
-        role = discord.utils.get(message.guild.roles, id = 683627319713071104) #Used to define the staff role
-        msg = role.mention + " Support Activated" #Used to mention the role
-        await channel.send(msg)
-        content = message.content
-        newcontent = content.replace("s", '#')
-        await channel.send(f"Triggered by: {newcontent}")
-        await channel.send("The s's were replaced with # for coding reasons.") #S is replaced to not trigger /support again and to stop duping.
-    await client.process_commands(message) #To allow other commands to function
+    if str(message.author) != "Support#4738":
+        if lock == 1:
+            if "?saylock" not in message.content.lower():
+                if prevmessage != message.content: #if the previous message is the same as the new one, nothing should hapen
+                    channel = client.get_channel(711137574642516008) #McChat Channel
+                    await channel.send(message.content) #Mimic Message
+                    prevmessage = message.content #Part of system to stop dupes
+                    await asyncio.sleep(2) #To Avoid bot overload
+        if "/support" in message.content.lower():
+            print("/support Activated")
+            channel = client.get_channel(710934053901303828)
+            role = discord.utils.get(message.guild.roles, id = 683627319713071104) #Used to define the staff role
+            msg = role.mention + " Support Activated" #Used to mention the role
+            await channel.send(msg)
+            content = message.content
+            newcontent = content.replace("s", '#')
+            await channel.send(f"Triggered by: {newcontent}")
+            await channel.send("The s's were replaced with # for coding reasons.") #S is replaced to not trigger /support again and to stop duping.
+        await client.process_commands(message) #To allow other commands to function
 
 @client.command()
 async def say(ctx, arg1, arg2):
@@ -45,17 +47,20 @@ async def say(ctx, arg1, arg2):
         await channel.send(f"{ctx.message.author}: {arg2}") #if arg1 is NOT s, it will show who send the message
 
 @client.command()
-async def saylock(ctx, arg1):
+async def saylock(ctx, arg1 = None):
     global lock
-    if arg1 == "Activate" or arg1 == "activate": #To set to activate mode
+    if arg1 == "-a" or arg1 == "-A": #To set to activate mode
         await ctx.send("Lock Activated")
-        await asyncio.sleep(3) #To stop Overload
         lock = 1 #Configure On_Message
-    else:
+    elif arg1 == "-d" or arg1 == "-D":
         await ctx.send("Lock Deactivated") #To set to deactivate mode
-        await asyncio.sleep(3) #To stop Overload
         lock = 0 #Configure On_Message
-
+    elif arg1 == None:
+        if lock == 1:
+            await ctx.send("SayLock is Currently On")
+        else:
+            await ctx.send("SayLock is Currently Off")
+            
 @client.command()
 async def ping(ctx):
     await ctx.send(f'My ping is {client.latency} (ms)!')
